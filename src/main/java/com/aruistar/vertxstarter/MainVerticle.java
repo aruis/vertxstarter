f10b0b096d54154d089f2fa2e45c7a3de41ae38d
Liu Rui <lovearuis@gmail.com>
2019-02-03 17:07:09 +0800

    add logback

--- src/main/java/com/aruistar/vertxstarter/MainVerticle.java
+++ src/main/java/com/aruistar/vertxstarter/MainVerticle.java
@@ -2,11 +2,16 @@ package com.aruistar.vertxstarter;
 
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Future;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class MainVerticle extends AbstractVerticle {
 
+  private static Logger logger = LoggerFactory.getLogger(MainVerticle.class);
+
   @Override
   public void start(Future<Void> startFuture) throws Exception {
+    logger.info("verticle start");
     vertx.createHttpServer().requestHandler(req -> {
       req.response()
         .putHeader("content-type", "text/plain")
