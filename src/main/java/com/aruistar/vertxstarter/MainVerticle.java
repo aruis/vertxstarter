cf56d54da2b8d48e0b0de3cfd6f5c41c63aad9a7
Liu Rui <lovearuis@gmail.com>
2019-02-08 07:52:27 +0800

    update counter

--- src/main/java/com/aruistar/vertxstarter/MainVerticle.java
+++ src/main/java/com/aruistar/vertxstarter/MainVerticle.java
@@ -1,66 +1,39 @@
 package com.aruistar.vertxstarter;
 
-import io.reactiverse.pgclient.PgClient;
-import io.reactiverse.pgclient.PgPool;
-import io.reactiverse.pgclient.PgPoolOptions;
-import io.reactiverse.pgclient.Tuple;
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Future;
-import io.vertx.core.http.HttpServerResponse;
-import io.vertx.core.json.JsonObject;
 import io.vertx.ext.web.Router;
-import io.vertx.ext.web.handler.BodyHandler;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.slf4j.bridge.SLF4JBridgeHandler;
 
 public class MainVerticle extends AbstractVerticle {
 
-  static {
-    SLF4JBridgeHandler.removeHandlersForRootLogger();
-    SLF4JBridgeHandler.install();
-  }
-
-  private static Logger logger = LoggerFactory.getLogger(MainVerticle.class);
+  int i = 0;
 
   @Override
   public void start(Future<Void> startFuture) throws Exception {
-    logger.info("verticle start");
-
-    PgPoolOptions options = new PgPoolOptions()
-      .setPort(5432)
-      .setHost("127.0.0.1")
-      .setDatabase("studypg")
-      .setUser("postgres")
-      .setPassword("secret")
-      .setMaxSize(5);
-
-// Create the client pool
-    PgPool client = PgClient.pool(vertx, options);
 
     Router router = Router.router(vertx);
 
-    router.post("/score")
-      .handler(BodyHandler.create())
+    router.get("/test")
       .handler(routingContext -> {
-        HttpServerResponse response = routingContext.response();
-        JsonObject json = routingContext.getBodyAsJson();
+        i++;
+        routingContext.response().end(i + "");
+      });
 
-        client.preparedQuery("insert into edu_score (v_lesson, n_score, v_name) values ($1,$2,$3) returning id;",
-          Tuple.of(json.getString("v_lesson"), json.getFloat("n_score"), json.getString("v_name")),
-          ar -> {
-            String id = ar.result().iterator().next().getString("id");
-            response.end(id);
-          });
+    router.get("/show")
+      .handler(routingContext -> {
+        routingContext.response().end(i + "");
+      });
+
+    router.get("/clean")
+      .handler(routingContext -> {
+        i = 0;
+        routingContext.response().end(i + "");
       });
 
     vertx.createHttpServer().requestHandler(router)
-      .listen(8080, http -> {
-        if (http.succeeded()) {
-          startFuture.complete();
-          System.out.println("HTTP server started on http://localhost:8080");
-        } else {
-          startFuture.fail(http.cause());
+      .listen(7070, ar -> {
+        if (ar.succeeded()) {
+          System.out.println("listen on 7070");
         }
       });
   }
